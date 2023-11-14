199f628919e8f3782e58411b0fe7b24752c9f2f1
Mihajlo Rajkovic <mikalo.rajkovic@gmail.com>
2023-11-14 20:29:42 +0100

    Prvi commit

---
+++ src/app/room/room.model.ts
@@ -0,0 +1,15 @@
+export class Room {
+    id: number;
+    name: string;
+    type: string;
+    price: number;
+    // Dodajte dodatne atribute po potrebi
+
+    constructor(id: number, name: string, type: string, price: number) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        // Inicijalizujte dodatne atribute po potrebi
+    }
+}
